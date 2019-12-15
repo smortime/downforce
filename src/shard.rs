71b991c7c7281d9180b3b6c1f7699a130f37da10
smortime <schuymortimer@gmail.com>
2019-12-15 15:34:04 -0800

    - change get_shard_count to use `fold`

--- src/shard.rs
+++ src/shard.rs
@@ -2,13 +2,13 @@ use crate::config::Config;
 use std::fs;
 use std::path::Path;
 
-// Represents the shard of work to be processed
+// Represents the shard of work to be processed during map phase
 pub struct Shard {
     pub id: i16,
     pub pieces: Vec<Piece>,
 }
 
-// Is the chunk of file that makes up a shard
+// Is a chunk of file that makes up a shard
 pub struct Piece {
     pub file_path: String,
     pub start: i32,
@@ -16,19 +16,21 @@ pub struct Piece {
 }
 
 pub fn get_shards(conf: &Config, shards: &mut Vec<Shard>) {
+    // loop through number of allocated shards
+    // loop through files
+    // create pieces based off size and add to shard
 
 }
 
 pub fn get_shard_count(conf: &Config) -> i16 {
-    let mut total_file_size: u64 = 0;
     let files = fs::read_dir(conf.input_dir.to_string())
         .expect("Unable to read input directory");
 
-    for file in files {
-        let metadata = fs::metadata(file
+    let total_file_size = files.fold(0, |sum, file|
+        sum + fs::metadata(file
             .expect("Invalid File in input directory")
-            .path()).unwrap();
-        total_file_size += metadata.len();
-    }
+            .path()).unwrap().len()
+    );
+
     (total_file_size / conf.shard_size as u64) as i16
 }
