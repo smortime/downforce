63053b80c0a78caceb35b1b9c10c5d3242271a7e
smortime <schuymortimer@gmail.com>
2019-12-31 12:41:14 -0800

    - add worker_status mutations o_O - clean up scoreboard

--- src/scoreboard.rs
+++ src/scoreboard.rs
@@ -1,13 +1,9 @@
 
+#[derive(PartialEq, Debug)]
 enum Status {
     Available,
     Busy,
     Failed,
-}
-
-struct Scoreboard {
-    id: i16,
-    worker_status: Vec<WorkerStatus>,
 }
 
 struct WorkerStatus {
@@ -15,28 +11,58 @@ struct WorkerStatus {
     status: Status,
 }
 
+impl WorkerStatus {
+    fn fail(&mut self) {
+        self.status = Status::Failed
+    }
+
+    fn busy(&mut self) {
+        self.status = Status::Busy
+    }
+
+    fn available(&mut self) {
+        self.status = Status::Available
+    }
+}
+
+struct Scoreboard {
+    id: i16,
+    workers: Vec<WorkerStatus>,
+}
+
 impl Scoreboard {
     fn new(id: i16, ports: &[i16]) -> Self {
         Scoreboard {
             id,
-            worker_status: Scoreboard::new_worker_status_vec(ports),
+            workers: Scoreboard::new_worker_status_vec(ports),
         }
     }
 
     fn new_worker_status_vec(ports: &[i16]) -> Vec<WorkerStatus> {
-        ports.iter().map(|&port|WorkerStatus{port, status: Status::Available}).collect()
+        ports.iter().map(|&port| WorkerStatus{port, status: Status::Available}).collect()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::SeekFrom::Start;
 
     #[test]
     fn validate_scoreboard_new() {
         let ports = vec![5001, 5002, 5003];
-        let scoreboard = Scoreboard::new(1, &ports);
+        let mut scoreboard = Scoreboard::new(1, &ports);
         assert_eq!(scoreboard.id, 1);
-        assert_eq!(scoreboard.worker_status.len(), ports.len());
+        assert_eq!(scoreboard.workers.len(), ports.len());
+    }
+
+    #[test]
+    fn validate_worker_status() {
+        let mut worker = WorkerStatus {port: 5001, status: Status::Available};
+        assert_eq!(worker.status, Status::Available);
+        worker.fail();
+        assert_eq!(worker.status, Status::Failed);
+        worker.busy();
+        assert_eq!(worker.status, Status::Busy);
     }
 }
